b9fffba93af5d739093099240dbaf7ca345ed2e4
TheFlav <freeplay@gmail.com>
2018-05-11 14:28:26 -0600

    Remove joy2key when done

--- Freeplay LCD Brightness.sh
+++ Freeplay LCD Brightness.sh
@@ -3,3 +3,4 @@ sudo killall joy2key.py
 /opt/retropie/supplementary/runcommand/joy2key.py /dev/input/js0 "0x44" "0x43" "0x43" "0x44" "0x20" "0x20" &
 /home/pi/Freeplay/setPCA9633/setPCA9633interactive D C ' ' 5 | dialog --gauge "Freeplay LCD Brightness" 6 30 0
 /home/pi/Freeplay/setPCA9633/setPCA9633 -y 1 -a 0x62 -e 0 -v 1 > /home/pi/Freeplay/setPCA9633/fpbrightness.val
+sudo killall joy2key.py
