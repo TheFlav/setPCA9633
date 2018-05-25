9ef9df189a5ac7742a14f7f6dd6567772b29970d
Matthew Krohn <matthewkrohn@7400s-MacBook-Pro.local>
2018-05-25 12:46:08 -0600

    modified:   setup.sh trying backslashes with brackets

--- setup.sh
+++ setup.sh
@@ -9,5 +9,5 @@ sudo mv /home/pi/freeplaycfg.txt /boot/freeplaycfg.txt
 mv Brightness.png /home/pi/RetroPie/retropiemenu/icons/Brightness.png
 
 sudo chmod 777 /boot/config.txt
-sudo sed 's|[pi3]|[pi3]\ndtparam=i2c1_baudrate=400000 #makes a big speed difference\ndtoverlay=i2c-1bcm2708,sda1_pin=45,pin_func=6\n[all]|' /boot/config.txt
+sudo sed 's|\[pi3\]|\[pi3\]\ndtparam=i2c1_baudrate=400000 #makes a big speed difference\ndtoverlay=i2c-1bcm2708,sda1_pin=45,pin_func=6\n[all]|' /boot/config.txt
 sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./"Freeplay LCD Brightness.sh"</path>\n\t\t<name>FreePlay Brightness Control</name>\n\t\t<desc>Control your screen brightness. Persists on reboot.</desc>\n\t\t<image>./icons/Brightness.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed></lastplayed>\n\t</game>\n\t</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
