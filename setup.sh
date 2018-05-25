6443e50f533a9cc7587c9c212e88533ffddfc48e
Matthew Krohn <matthewkrohn@7400s-MacBook-Pro.local>
2018-05-25 12:44:17 -0600

    modified:   setup.sh missing >

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ make
 mv "Freeplay LCD Brightness.sh" /home/pi/RetroPie/retropiemenu/"Freeplay LCD Brightness.sh"
 sudo cp /boot/freeplaycfg.txt /home/pi/freeplaycfg.txt
 sudo chmod 777 /home/pi/freeplaycfg.txt
-echo "/home/pi/Freeplay/setPCA9633/setFreeplayBacklight.sh >> /dev/null" > /home/pi/freeplaycfg.txt
+echo "/home/pi/Freeplay/setPCA9633/setFreeplayBacklight.sh >> /dev/null" >> /home/pi/freeplaycfg.txt
 sudo mv /boot/freeplaycfg.txt /boot/freeplaycfg.bak
 sudo mv /home/pi/freeplaycfg.txt /boot/freeplaycfg.txt
 mv Brightness.png /home/pi/RetroPie/retropiemenu/icons/Brightness.png
