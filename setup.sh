86dcd518eb6d821129be4ff238da64b855a5c069
Matthew Krohn <matthew.d.krohn@gmail.com>
2018-06-13 08:54:25 -0600

    Made all commands run sudo, also kills and restarts emulationstation

--- setup.sh
+++ setup.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 make
+sudo killall emulationstation
 cp "Freeplay LCD Brightness.sh" /home/pi/RetroPie/retropiemenu/"Freeplay LCD Brightness.sh"
 sudo cp /boot/freeplaycfg.txt /home/pi/freeplaycfg.txt
 sudo sh -c 'echo "/home/pi/Freeplay/setPCA9633/setFreeplayBacklight.sh > /dev/null" >> /home/pi/freeplaycfg.txt'
 sudo mv /boot/freeplaycfg.txt /boot/freeplaycfg.bak
 sudo mv /home/pi/freeplaycfg.txt /boot/freeplaycfg.txt
-cp Brightness.png /home/pi/RetroPie/retropiemenu/icons/Brightness.png
+sudo cp Brightness.png /home/pi/RetroPie/retropiemenu/icons/Brightness.png
 
 sudo sh -c 'echo "[pi3]\ndtparam=i2c1_baudrate=400000 #makes a big speed difference\ndtoverlay=i2c1-bcm2708,sda1_pin=44,scl1_pin=45,pin_func=6\n" >> /boot/config.txt'
 sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./"Freeplay LCD Brightness.sh"</path>\n\t\t<name>FreePlay Brightness Control</name>\n\t\t<desc>Control your screen brightness. Persists on reboot.</desc>\n\t\t<image>./icons/Brightness.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed></lastplayed>\n\t</game>\n\t</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+emulationstation
