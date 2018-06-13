3ae39ec5f204325f42a8d316442ec47cb3c7d5a5
Matthew Krohn <matthew.d.krohn@gmail.com>
2018-06-13 11:20:39 -0600

    One more sudo

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@ make
 cp Freeplay\ LCD\ Brightness.sh /home/pi/RetroPie/retropiemenu/Freeplay\ LCD\ Brightness.sh
 sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./Freeplay LCD Brightness.sh</path>\n\t\t<name>FreePlay Brightness Control</name>\n\t\t<desc>Control your screen brightness. Persists on reboot.</desc>\n\t\t<image>./icons/Brightness.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed></lastplayed>\n\t</game>\n\t</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 
-cp /boot/freeplaycfg.txt /home/pi/freeplaycfg.txt
+sudo cp /boot/freeplaycfg.txt /home/pi/freeplaycfg.txt
 sudo sh -c 'echo "/home/pi/Freeplay/setPCA9633/setFreeplayBacklight.sh > /dev/null" >> /home/pi/freeplaycfg.txt'
 sudo mv /boot/freeplaycfg.txt /boot/freeplaycfg.bak
 sudo mv /home/pi/freeplaycfg.txt /boot/freeplaycfg.txt
