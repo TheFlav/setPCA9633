5a7fff2331647ca3a45701b5cf39237262db9ac2
Matthew Krohn <matthew.d.krohn@gmail.com>
2018-06-18 16:08:57 -0600

    Added message that explains that an extra controller is needed

--- Freeplay LCD Brightness.sh
+++ Freeplay LCD Brightness.sh
@@ -6,6 +6,6 @@ then
 	/home/pi/Freeplay/setPCA9633/setPCA9633interactive D C ' ' 5 | dialog --gauge "Freeplay LCD Brightness" 6 30 0
 	/home/pi/Freeplay/setPCA9633/setPCA9633 -y 1 -a 0x62 -e 0 -v 1 > /home/pi/Freeplay/setPCA9633/fpbrightness.val
 else
-	dialog --msgbox "I2C has not been enabled yet. Go to RetroPie Menu, enter the Raspi Config, and then turn on I2C in the Devices/Interfaces section." 15 40 
+	dialog --msgbox "I2C has not been enabled yet. Go to RetroPie Menu, enter the Raspi Config, and then turn on I2C in the Devices/Interfaces section. This will only work if you have a controller installed." 15 40 
 fi
 sudo killall joy2key.py
