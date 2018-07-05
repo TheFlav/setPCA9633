b5d2ffde0e8e6612de59c550768c5b4adf7e0dba
Matthew Krohn <matthew.d.krohn@gmail.com>
2018-07-05 16:08:27 -0600

    Removed unnecessary copy

--- setup.sh
+++ setup.sh
@@ -4,7 +4,6 @@ cp Freeplay\ LCD\ Brightness.sh /home/pi/RetroPie/retropiemenu/Freeplay\ LCD\ Br
 sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./Freeplay LCD Brightness.sh</path>\n\t\t<name>Freeplay Brightness Control</name>\n\t\t<desc>Control your screen brightness. Persists on reboot.</desc>\n\t\t<image>./icons/Brightness.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed></lastplayed>\n\t</game>\n\t</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 
 sudo cp /boot/freeplaycfg.txt /home/pi/freeplaycfg.txt
-sudo cp ./fpbrightness.val /home/pi/Freeplay/setPCA9633/fpbrightness.val
 sudo sh -c 'printf "\n/home/pi/Freeplay/setPCA9633/setFreeplayBacklight.sh > /dev/null" >> /home/pi/freeplaycfg.txt'
 sudo mv /boot/freeplaycfg.txt /boot/freeplaycfg.bak
 sudo mv /home/pi/freeplaycfg.txt /boot/freeplaycfg.txt
