3ae10b008080c57333e1d7b1e4be59323e2d3a92
Matthew Krohn <matthew.d.krohn@gmail.com>
2019-05-14 14:12:39 -0600

    updated with two if statements to check if sed commands have already been run

--- setup.sh
+++ setup.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 make
 cp Freeplay\ LCD\ Brightness.sh /home/pi/RetroPie/retropiemenu/Freeplay\ LCD\ Brightness.sh
-sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./Freeplay LCD Brightness.sh</path>\n\t\t<name>Freeplay Brightness Control</name>\n\t\t<desc>Control your screen brightness. Persists on reboot.</desc>\n\t\t<image>./icons/Brightness.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed></lastplayed>\n\t</game>\n\t</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+if  grep -q "Freeplay LCD Brightness" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
+	echo "Brightness already in RetroPie Menu"
+else
+	sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./Freeplay LCD Brightness.sh</path>\n\t\t<name>Freeplay Brightness Control</name>\n\t\t<desc>Control your screen brightness. Persists on reboot.</desc>\n\t\t<image>./icons/Brightness.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed></lastplayed>\n\t</game>\n\t</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
+fi
 
-sudo sed -i 's|exit 0|/home/pi/Freeplay/setPCA9633/setFreeplayBacklight.sh > /dev/null\n\nexit 0|' /etc/rc.local
+if  grep -q "setFreeplayBacklight.sh" /etc/rc.local ; then
+	echo "Backlight already setup"
+else
+	sudo sed -i 's|exit 0|/home/pi/Freeplay/setPCA9633/setFreeplayBacklight.sh > /dev/null\n\nexit 0|' /etc/rc.local
+fi
 
 cp Brightness.png /home/pi/RetroPie/retropiemenu/icons/Brightness.png
 
