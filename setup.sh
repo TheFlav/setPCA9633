89b0b19d0476c7d204a94f4cdb800d9b1d8caf1d
Matthew Krohn <matthew.d.krohn@gmail.com>
2019-05-28 14:54:11 -0600

    Changed to new install method (directory parsing, greping)

--- setup.sh
+++ setup.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
+
+INSTALL_DIR=/home/pi/Freeplay/$(ls /home/pi/Freeplay | grep -i setpca9633)
+cd $INSTALL_DIR
+
 make
-cp Freeplay\ LCD\ Brightness.sh /home/pi/RetroPie/retropiemenu/Freeplay\ LCD\ Brightness.sh
+cp $INSTALL_DIR/Freeplay\ LCD\ Brightness.sh /home/pi/RetroPie/retropiemenu/Freeplay\ LCD\ Brightness.sh
+
 if  grep -q "Freeplay LCD Brightness" /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml ; then
-	echo "Brightness already in RetroPie Menu"
+	echo "Brightness Control has already been added to RetroPie Menu"
 else
 	sudo sed -i 's|</gameList>|\t<game>\n\t\t<path>./Freeplay LCD Brightness.sh</path>\n\t\t<name>Freeplay Brightness Control</name>\n\t\t<desc>Control your screen brightness. Persists on reboot.</desc>\n\t\t<image>./icons/Brightness.png</image>\n\t\t<playcount>0</playcount>\n\t\t<lastplayed></lastplayed>\n\t</game>\n\t</gameList>|' /opt/retropie/configs/all/emulationstation/gamelists/retropie/gamelist.xml
 fi
 
 if  grep -q "setFreeplayBacklight.sh" /etc/rc.local ; then
-	echo "Backlight already setup"
+	echo "Brightness Control has already been added to rc.local"
 else
 	sudo sed -i 's|^exit 0|/home/pi/Freeplay/setPCA9633/setFreeplayBacklight.sh > /dev/null\n\nexit 0|' /etc/rc.local
 fi
 
-cp Brightness.png /home/pi/RetroPie/retropiemenu/icons/Brightness.png
+cp $INSTALL_DIR/Brightness.png /home/pi/RetroPie/retropiemenu/icons/Brightness.png
 
 sudo sh -c 'printf "[pi3]\ndtparam=i2c1_baudrate=400000 #makes a big speed difference\ndtoverlay=i2c1-bcm2708,sda1_pin=44,scl1_pin=45,pin_func=6,combine=off\n[all]" >> /boot/config.txt'
